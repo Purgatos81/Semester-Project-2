5385169236a321b4272ca851f38c7b68ab928f63
John HC <69842878+Purgatos81@users.noreply.github.com>
2021-12-02 14:37:20 +0100

    edit: added add and remove to cart button to details page

--- js/detail.js
+++ js/detail.js
@@ -1,6 +1,7 @@
 import { baseUrl } from "./settings/api.js";
 import displayMessage from "./components/common/displayMessage.js";
 import createLogin from "./components/common/createNav.js";
+import { imgBasicUrl } from "./settings/api.js";
 
 createLogin();
 
@@ -27,10 +28,25 @@ console.log(productUrl);
 
         const container = document.querySelector(".detail-container");
 
-        container.innerHTML = `<h1>${details.title}</h1>
-                                <p>${details.description}</p>`;
+        container.innerHTML = `<h2 class="details-h2">${details.title}</h1>
+                                <p class="details-p">${details.description}</p>
+                                <img class="details-img" src="${imgBasicUrl + details.image_url}">
+                                <p class="details-price-p">$ ${details.price}</p>
+                                <div class="cart-button-container">
+                                <i class="fas fa-cart-plus"></i>
+                                </div>`;
 
+                                const addButton = document.querySelectorAll(".cart-button-container i");
 
+                                addButton.forEach((button) => {
+                                    button.addEventListener("click", handleClick);
+                                })
+                                
+                                function handleClick() {
+                                    console.log(event);
+                                    event.target.classList.toggle("fa-trash-alt");
+                                    event.target.classList.toggle("fa-cart-plus");
+                                }
         console.log(details);
     } catch (error) {
         displayMessage("error", error, ".detail-container");
