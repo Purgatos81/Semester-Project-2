50e206acf578abc0c872b81e5da91f9a40734596
John HC <69842878+Purgatos81@users.noreply.github.com>
2021-12-07 20:38:35 +0100

    edit:fixed js on search option

--- js/products.js
+++ js/products.js
@@ -5,23 +5,25 @@ import { imgBasicUrl } from "./settings/api.js";
 import { getUsername } from "./utils/storage.js";
 
 const productsUrl = baseUrl + "products";
-const search = document.querySelector("search");
+const search = document.querySelector(".search");
 const username = getUsername();
+
 
 createLogin();
 
 console.log(productsUrl);
 
-(async function () {
+async function prodCall() {
     const container = document.querySelector(".products-container");
 
     try {
         const response = await fetch(productsUrl);
         const json = await response.json();
+        let bson = json;
 
         container.innerHTML = "";
 
-        json.forEach(function (product) {
+        bson.forEach(function (product) {
             if(username) {
                 container.innerHTML += `<a class="featured-product" href="edit.html?id=${product.id}">
                                         <img class="featured-img" src="${imgBasicUrl + product.image_url}">
@@ -35,12 +37,88 @@ console.log(productsUrl);
                                         <h4 class="product-h4" >${product.title}</h4>
                                         <p class="product-p" >Price: ${product.price}</p>
                                         </a></div>`;
-            }                        
-        });
+            }     
+            search.onkeyup = function () {
+                // console.log(event);
+            
+                const searchValue = event.target.value.trim().toLowerCase();
+            
+                const filteredProducts = json.filter(function (prod) {
+                    if (prod.title.toLowerCase().startsWith(searchValue)) {
+                        return true;
+                    }
+                });
+            
+                console.log(filteredProducts);
+            
+                bson = filteredProducts;
 
-        console.log(json);
+                container.innerHTML = "";
+
+                bson.forEach(function (product) {
+                    if(username) {
+                        container.innerHTML += `<a class="featured-product" href="edit.html?id=${product.id}">
+                                                <img class="featured-img" src="${imgBasicUrl + product.image_url}">
+                                                <h4 class="featured-h4">${product.title}</h4>
+                                                <p class="featured-p">$ ${product.price}</p>
+                                                </a>`;
+                    } else {
+                        container.innerHTML += `<div class="product-card-container">
+                                                <a class="product-card" href="details.html?id=${product.id}">
+                                                <img class="product-img" src="${imgBasicUrl + product.image_url}">
+                                                <h4 class="product-h4" >${product.title}</h4>
+                                                <p class="product-p" >Price: ${product.price}</p>
+                                                </a></div>`;
+                    }    
+            
+            });
+            // console.log(json);
+            // prodCall();                
+        }});
+
     } catch (error) {
         console.log(error);
         displayMessage("error", error, ".products-container");
     }
-})();
+
+};
+
+prodCall();
+
+//         search.onkeyup = function () {
+//             console.log(event);
+        
+//             const searchValue = event.target.value.trim().toLowerCase();
+        
+//             const filteredProducts = json.filter(function (prod) {
+//                 if (prod.title.toLowerCase().startsWith(searchValue)) {
+//                     return true;
+//                 }
+//             });
+        
+//             console.log(filteredProducts);
+        
+//             // productsUrl = filteredProducts;
+        
+//             prodCall();
+        
+//         };
+
+// search.onkeyup = function () {
+//     console.log(event);
+
+//     const searchValue = event.target.value.trim().toLowerCase();
+
+//     const filteredProducts = baseUrl.filter(function (prod) {
+//         if (prod.title.toLowerCase().startsWith(searchValue)) {
+//             return true;
+//         }
+//     });
+
+//     console.log(filteredProducts);
+
+//     productsToRender = filteredProducts;
+
+//     prodCall();
+
+// };
